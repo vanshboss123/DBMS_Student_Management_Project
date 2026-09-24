9a663f07e8f9bb12b64996493ad34f5c514e3b3c
vanshboss123 <vanshbosssrahate@gmail.com>
2026-09-24 14:41:24 +0530

    Delete DBMS_Student_Management_Project directory

--- DBMS_Student_Management_Project/public/config.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Copy this file to config.php and edit the credentials for your machine.
-return [
-    'host' => '127.0.0.1',
-    'port' => 3306,
-    'database' => 'student_management',
-    'username' => 'root',
-    'password' => '',
-];
--- DBMS_Student_Management_Project/public/partials/footer.php
+++
@@ -1,4 +0,0 @@
-</main>
-<footer class="footer">Student Information Management System | MySQL 8.0+ | DBMS Lab Mini Project</footer>
-</body>
-</html>
